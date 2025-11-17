9e3a0a88f101b9446cb32580a542d082cc0c468f
pawover <pawover@outlook.com>
2025-11-17 10:20:12 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-import javascript from "./core.javascript.js";
-import typescript from "./core.typescript.js";
+import javascriptRules from "./core.javascript.js";
+import typescriptRules from "./core.typescript.js";
 
-import antfu from "./core.antfu.js";
-import react from "./core.react.js";
-import stylistic from "./core.stylistic.js";
-import vue from "./core.vue.js";
+import antfuRules from "./core.antfu.js";
+import reactRules from "./core.react.js";
+import stylisticRules from "./core.stylistic.js";
+import vueRules from "./core.vue.js";
+
+export type SeverityLevel = 0 | 1 | 2;
+export type SeverityName = "off" | "warn" | "error";
+export type Severity = SeverityName | SeverityLevel;
+export type RuleConfig = Severity | [Severity];
+
+const javascript = javascriptRules as unknown as Record<keyof typeof javascriptRules, RuleConfig>;
+const typescript = typescriptRules as unknown as Record<keyof typeof typescriptRules, RuleConfig>;
+const react = reactRules as unknown as Record<keyof typeof reactRules, RuleConfig>;
+const vue = vueRules as unknown as Record<keyof typeof vueRules, RuleConfig>;
+const stylistic = stylisticRules as unknown as Record<keyof typeof stylisticRules, RuleConfig>;
+const antfu = antfuRules as unknown as Record<keyof typeof antfuRules, RuleConfig>;
 
 export default {
   javascript,
