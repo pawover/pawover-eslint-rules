7e80cb4eea12041d37cc61cfdc9147c168e4f006
pawover <pawover@outlook.com>
2026-04-07 16:39:39 +0800

    add

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import vueRules from "./core.vue.js";
 export type SeverityLevel = 0 | 1 | 2;
 export type SeverityName = "off" | "warn" | "error";
 export type Severity = SeverityName | SeverityLevel;
-export type RuleConfig = Severity | [Severity];
+export type RuleConfig<RuleOptions extends unknown[] = unknown[]> = Severity | [Severity, ...Partial<RuleOptions>];
 
 const javascript = javascriptRules as unknown as Record<keyof typeof javascriptRules, RuleConfig>;
 const typescript = typescriptRules as unknown as Record<keyof typeof typescriptRules, RuleConfig>;
